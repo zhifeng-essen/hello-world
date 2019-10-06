334438b69b780ac8028bb5af548438c1b0a15e63
zhifeng-essen <54047165+zhifeng-essen@users.noreply.github.com>
2019-10-06 19:23:41 +0800

    Initial Commit

---
+++ hello-world/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello-world
+//
+//  Created by Essen on 2019/10/6.
+//  Copyright © 2019 Essen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
